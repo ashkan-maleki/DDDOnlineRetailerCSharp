d973923aa9b68c24c4cda15fa8ba8c4705598a59
ma.maleki <ma.maleki@PC-1293>
2024-03-11 10:31:23 +0330

    Add allocation mechanism to our little retailer.

---
+++ DDDOnlineRetailerCSharp/Domain/Domain.cs
@@ -0,0 +1,38 @@
+﻿namespace DDDOnlineRetailerCSharp.Domain;
+
+public class OutOfStock : Exception
+{
+    public OutOfStock()
+    {
+    }
+
+    public OutOfStock(string message)
+        : base(message)
+    {
+    }
+
+    public OutOfStock(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
+public class Domain
+{
+    public static string Allocate(OrderLine line, IEnumerable<Batch> batches)
+    {
+        Batch? batch = batches.Order().FirstOrDefault(b => b.CanAllocate(line));
+        
+        if (batch == null)
+        {
+            Console.WriteLine("batch is null");
+            throw new OutOfStock($"Out of stock for sku {line.Sku}");
+        }
+        else
+        {
+            Console.WriteLine("batch is not null");
+            Console.WriteLine(batch);
+        }
+        batch.Allocate(line);
+        return batch.Reference!;
+    }
+}
--- DDDOnlineRetailerCSharp/Domain/Model.cs
+++ DDDOnlineRetailerCSharp/Domain/Model.cs
@@ -1,47 +1,29 @@
 ﻿namespace DDDOnlineRetailerCSharp.Domain;
 
-public class OutOfStock : Exception
-{
-    public OutOfStock()
-    {
-    }
 
-    public OutOfStock(string message)
-        : base(message)
-    {
-    }
-
-    public OutOfStock(string message, Exception inner)
-        : base(message, inner)
-    {
-    }
-}
 
 public record OrderLine(string OrderId, string Sku, int Qty);
 
-public class Batch(string? reference, string? sku, int purchasedQuantity, DateTime? eta = null)
+public class Batch(string? reference, string? sku, int purchasedQuantity, DateTime? eta = null) : IComparable<Batch>
 {
-    public string? Reference => _reference;
-
-    public string? Sku => _sku;
-
-    public DateTime? Eta => _eta;
-
-    public int PurchasedQuantity => _purchasedQuantity;
-
-    public List<OrderLine> Allocations => _allocations;
-
     private string? _sku = sku;
     private DateTime? _eta = eta;
     private int _purchasedQuantity = purchasedQuantity;
     private List<OrderLine> _allocations = new();
     private readonly string? _reference = reference;
-
+    
+    
+    public string? Reference => _reference;
+    public string? Sku => _sku;
+    public DateTime? Eta => _eta;
+    public int PurchasedQuantity => _purchasedQuantity;
+    public List<OrderLine> Allocations => _allocations;
     public int AllocatedQuantity => Allocations.Sum(ol => ol.Qty);
     public int AvailableQuantity => PurchasedQuantity - AllocatedQuantity;
 
-    public bool CanAllocate(OrderLine line) => Sku == line.Sku && PurchasedQuantity >= line.Qty;
-
+    
+    
+    public bool CanAllocate(OrderLine line) => Sku == line.Sku && AvailableQuantity >= line.Qty;
     public void Allocate(OrderLine line)
     {
         if (CanAllocate(line))
@@ -49,7 +31,6 @@ public class Batch(string? reference, string? sku, int purchasedQuantity, DateTi
             _allocations.Add(line);
         }
     }
-
     public void Deallocate(OrderLine line)
     {
         if (_allocations.Contains(line))
@@ -60,6 +41,18 @@ public class Batch(string? reference, string? sku, int purchasedQuantity, DateTi
 
 
     public override string ToString() => $"<Batch {Reference}>";
+    public int CompareTo(Batch? other)
+    {
+        // A null value means that this object is smaller.
+        if (Eta == null)
+            return -1;
+        
+        // A null value means that this object is greater.
+        if (other == null || other.Eta == null)
+            return 1;
+
+        return Eta.Value.CompareTo(other.Eta.Value);
+    }
 
     public override bool Equals(object? obj)
     {
@@ -70,20 +63,16 @@ public class Batch(string? reference, string? sku, int purchasedQuantity, DateTi
 
         return false;
     }
-
     public override int GetHashCode() => HashCode.Combine(Reference);
-
+    
     public static bool operator ==(Batch? lhs, Batch? rhs)
     {
         if (lhs is null)
         {
             return rhs is null;
         }
-
-        // Equals handles case of null on right side.
         return lhs.Equals(rhs);
     }
-
     public static bool operator >(Batch lhs, Batch rhs)
     {
         if (lhs.Eta is null)
@@ -98,8 +87,6 @@ public class Batch(string? reference, string? sku, int purchasedQuantity, DateTi
 
         return lhs.Eta > rhs.Eta;
     }
-
     public static bool operator <(Batch lhs, Batch rhs) => rhs > lhs;
-
     public static bool operator !=(Batch lhs, Batch rhs) => !(lhs == rhs);
 }
