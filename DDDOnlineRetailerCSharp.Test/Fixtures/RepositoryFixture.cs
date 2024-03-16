dfb1c37d192cc11176dc6952d5e1d76111c3e6e0
ma.maleki <ma.maleki@PC-1293>
2024-03-16 14:46:46 +0330

    add service layer

---
+++ DDDOnlineRetailerCSharp.Test/Fixtures/RepositoryFixture.cs
@@ -0,0 +1,25 @@
+﻿using DDDOnlineRetailerCSharp.Link;
+using DDDOnlineRetailerCSharp.Persistence;
+
+namespace DDDOnlineRetailerCSharp.Test.Fixtures;
+
+public class RepositoryFixture : IDisposable
+{
+    public RepositoryFixture()
+    {
+        DbContext = RetailerDbContext.CreateSqliteRetailerDbContext();
+        UnitOfWork = new UnitOfWork(DbContext);
+        Repository = new Repository(DbContext);
+    }
+
+    public RetailerDbContext DbContext { get; }
+
+    public IUnitOfWork UnitOfWork { get; }
+
+    public IRepository Repository { get; }
+
+    public void Dispose()
+    {
+        DbContext.Dispose();
+    }
+}
---
+++ DDDOnlineRetailerCSharp/Presentation/Services/BatchService.cs
@@ -0,0 +1,39 @@
+﻿using DDDOnlineRetailerCSharp.Domain;
+using DDDOnlineRetailerCSharp.Link;
+
+namespace DDDOnlineRetailerCSharp.Presentation.Services;
+
+public class InvalidSku : Exception
+{
+    public InvalidSku()
+    {
+    }
+
+    public InvalidSku(string message)
+        : base(message)
+    {
+    }
+
+    public InvalidSku(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
+
+public class BatchService
+{
+    private bool IsValidSku(string sku, List<Batch> batches) => batches.Any(b => b.Sku == sku);
+
+    public async Task<string> Allocate(OrderLine line, IRepository repo, IUnitOfWork unitOfWork)
+    {
+        List<Batch> batches = await repo.ListAsync();
+        if (!IsValidSku(line.Sku, batches))
+        {
+            throw new InvalidSku($"Invalid sku {line.Sku}");
+        }
+
+        string batchRef = Domain.Domain.Allocate(line, batches);
+        await unitOfWork.CommitAsync();
+        return batchRef;
+    }
+}
