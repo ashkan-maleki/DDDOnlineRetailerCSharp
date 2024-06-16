fee3678391b9230d1bf5de528db02d0e76fda75b
ma.maleki <ma.maleki@PC-1293>
2024-06-16 13:47:08 +0430

    add allocation view

---
+++ DDDOnlineRetailerCSharp/Domain/AllocationView.cs
@@ -0,0 +1,8 @@
+﻿namespace DDDOnlineRetailerCSharp.Domain;
+
+public class AllocationView(string sku, string orderId, string batchRef)
+{
+    public string? Sku { get; } = sku;
+    public string? OrderId { get; } = orderId;
+    public string? BatchRef { get; } = batchRef;
+}
---
+++ DDDOnlineRetailerCSharp/Persistence/Entities/AllocationViewTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using DDDOnlineRetailerCSharp.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DDDOnlineRetailerCSharp.Persistence.Entities;
+
+public class AllocationViewTypeConfiguration : IEntityTypeConfiguration<AllocationView>
+{
+    public void Configure(EntityTypeBuilder<AllocationView> builder)
+    {
+        builder.ToTable("allocations_view");
+
+        builder.Property<string>(b => b.OrderId!)
+            .UsePropertyAccessMode(PropertyAccessMode.Field)
+            .IsRequired();
+
+        builder.Property<string>(b => b.Sku!)
+            .UsePropertyAccessMode(PropertyAccessMode.Field)
+            .IsRequired();
+
+
+        builder.Property<string?>(b => b.BatchRef)
+            .UsePropertyAccessMode(PropertyAccessMode.Field)
+            .IsRequired(false);
+
+    }
+
+    
+}
